bd9d0fa55edfe2b30059210223736fe01af5175f
Yuhan <yuhancao233@gmail.com>
2025-06-05 15:41:31 +0100

    Add docker file

--- setup.sh
+++ setup.sh
@@ -70,7 +70,8 @@ fi
 if [ "$NEW_ENV" = true ] ; then
     conda create -n trellis python=3.10
     conda activate trellis
-    conda install pytorch==2.4.0 torchvision==0.19.0 pytorch-cuda=11.8 -c pytorch -c nvidia
+    # conda install pytorch==2.4.0 torchvision==0.19.0 pytorch-cuda=11.8 -c pytorch -c nvidia
+    conda install pytorch==2.5.0 torchvision==0.20.0 pytorch-cuda=12.4 -c pytorch -c nvidia
 fi
 
 # Get system information
